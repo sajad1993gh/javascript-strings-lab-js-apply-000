7e1cab945dd227d69b4b52490b0ba883f171ef5d
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-24 21:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = `Let's talk about ${topic}.`;
+var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}`;
